b34d5cf590609c7fb58c273d14c0557242adbe14
90541111262 <133885097+90541111262@users.noreply.github.com>
2024-06-08 16:46:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!-- /index.php -->
-<?php include 'includes/header.php'; ?>
+<?php include 'header.php'; ?>
  <main>
     <h1>Welcome to my First Footbal PHP Website</h1>
     <p>This is the Home Page of the Footbal Lovers</p>
  </main>
- <?php include 'includes/footer.php'; ?>
+ <?php include 'footer.php'; ?>
