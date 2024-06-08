5ff277d72c2b1d8ddcbf25dca1ea50a2f0f9c4ec
90541111262 <133885097+90541111262@users.noreply.github.com>
2024-06-08 16:40:44 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- /includes/footer.php -->
+<footer>
+        <p>&copy; <?= date("y") ?> My First FOOTBAL PHP Website. All rights Reserved ©.</p>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- /index.php -->
+<?php include 'includes/header.php'; ?>
+ <main>
+    <h1>Welcome to my First Footbal PHP Website</h1>
+    <p>This is the Home Page of the Footbal Lovers</p>
+ </main>
+ <?php include 'includes/footer.php'; ?>
